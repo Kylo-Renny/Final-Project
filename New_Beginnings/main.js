e3f6c2cad9b214574dbceea7ab4204ea388218b3
Ren <146970873+Kylo-Renny@users.noreply.github.com>
2023-12-08 08:35:41 -0800

    forced data array

--- New_Beginnings/main.js
+++ New_Beginnings/main.js
@@ -1,3 +1,150 @@
 
-	// New goals 
-    // 
+	// main.js - final project
+    // Author:
+    // Date:12/11/23
+    // New goals 
+    //Create an Array of data with callbacks that activate when buttons are pushed
+    //Created with heavy reference from Wes' "Choose Your Own Adventure" game
+//////////////////////////////////////////////////////////////////////////////////
+
+//Create a data object in which all of the game information is stored
+//the data
+rooms = {
+    closedinvitation: {
+        name: "Your Welcome Letter",
+        description: "script",
+        exitKeys: ["openinvitation"],
+        exitTexts:["Oh woah!"],
+        image: "closedletter.png"
+    },
+
+    openinvitation: {
+        name: "",
+        description: "",
+        exitKeys: [""],
+        exitTexts: [""],
+        image: "openletter.png"
+    },
+
+    stayhome: {
+        name:
+        description:
+        exitKeys:
+        exitTexts:
+        image:
+    },
+
+    goparty: {
+        name:
+        description:
+        exitKeys:
+        exitTexts:
+        image:
+    },
+
+    checkmap: {
+        name:
+        description:
+        exitKeys:
+        exitTexts:
+        image:
+    },
+
+    noafterparty: {
+        name:
+        description:
+        exitKeys:
+        exitTexts:
+        image:
+    },
+
+    goafterparty: {
+        name:
+        description:
+        exitKeys:
+        exitTexts:
+        image:
+    },
+
+    cands: {
+        name:
+        description:
+        exitKeys:
+        exitTexts:
+        image:
+    },
+
+    merrill: {
+        name:
+        description:
+        exitKeys:
+        exitTexts:
+        image:
+    },
+
+    crown: {
+        name:
+        description:
+        exitKeys:
+        exitTexts:
+        image:
+    },
+
+    c9: {
+        name:
+        description:
+        exitKeys:
+        exitTexts:
+        image:
+    },
+
+    jrl: {
+        name:
+        description:
+        exitKeys:
+        exitTexts:
+        image:
+    },
+
+    kresge: {
+        name:
+        description:
+        exitKeys:
+        exitTexts:
+        image:
+    },
+
+    porter: {
+        name:
+        description:
+        exitKeys:
+        exitTexts:
+        image:
+    },
+
+    portermeadows: {
+        name:
+        description:
+        exitKeys:
+        exitTexts:
+        image:
+    },
+
+    takedrugs: {
+        name:
+        description:
+        exitKeys:
+        exitTexts:
+        image:
+    },
+
+    nodrugs: {
+        name:
+        description:
+        exitKeys:
+        exitTexts:
+        image:
+    },
+
+
+}
