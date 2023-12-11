9e01295882b1dae61971a1264b2f1d0d5e6fe226
scarlett-crypto <129878971+scarlett-crypto@users.noreply.github.com>
2023-12-11 12:08:08 -0800

    undefined

--- New_Beginnings/main.js
+++ New_Beginnings/main.js
@@ -46,7 +46,7 @@ rooms = {
         name: "Map of Campus",
         description: "Nice work. You've been invited to a party at Oakes college, it's too late to catch the loop bus, so you'll have to walk.",
         exitKeys: ["goafterparty", "noafterparty"],
-        exitTexts: ["Whoa! This campus is massive! Guess I can skip leg day from now on."],
+        exitTexts: ["Whoa! This campus is massive! Guess I can skip leg day from now on.", "Walk home."],
         // IDK HOW TO ADD AN ALT TEXT TO CREDIT UCSC FOR THE MAP... -jazmine
         // We can add that to sources on our final doc, nt here -scarlett
         image:"map.gif"
