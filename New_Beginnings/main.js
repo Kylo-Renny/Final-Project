0f83efb38fadf8a6ca27260c6ea6ce580ddcf11e
Ren <146970873+Kylo-Renny@users.noreply.github.com>
2023-12-11 15:53:38 -0800

    final push

--- New_Beginnings/main.js
+++ New_Beginnings/main.js
@@ -15,8 +15,8 @@ rooms = {
         description: "You've received your invitation to UCSC Welcome Week! ",
         exitKeys: ["openinvitation"],
         exitTexts:["Oh whoa, let's see what's in the letter!"],
-        image: "0closedletter.gif",
-        button: ""
+        image: "0closedletter.gif"
+        //button: ""
     },
 
     openinvitation: {
@@ -24,8 +24,8 @@ rooms = {
         description: "Hey there slug, welcome to UCSC. Come on down to the East Remote lot field to get started",
         exitKeys: ["stayhome","goparty"],
         exitTexts: ["I'd rather stay home","Sure! That sounds fun!"],
-        image: "0openletter.gif",
-        button: ""
+        image: "0openletter.gif"
+        //button: ""
     },
 
     stayhome: {
@@ -33,8 +33,8 @@ rooms = {
         description:" 'Damn college is lonely- I guess I'll go to bed' *lights out* ",
         exitKeys: ["closedinvitation"],
         exitTexts:["Restart"],
-        image: "1.0home.gif",
-        button: ""
+        image: "1.0home.gif"
+        //button: ""
     },
 
     goparty: {
@@ -42,8 +42,8 @@ rooms = {
         description:"Hey Slug! Welcome to UCSC to get started, head on down to the cornucopia at the east field to find your map.",
         exitKeys: ["checkmap"],
         exitTexts: ["Oh Sweet! I can't wait to meet my fellow slugs!"],
-        image:"1.1party.gif",
-        button: ""
+        image:"1.1party.gif"
+        //button: ""
     },
 
     checkmap: {
@@ -53,8 +53,8 @@ rooms = {
         exitTexts: ["Whoa! This campus is massive! Guess I can skip leg day from now on.", "Nah, you've had enough social interaction and you decide to Walk home."],
         // IDK HOW TO ADD AN ALT TEXT TO CREDIT UCSC FOR THE MAP... -jazmine
         // We can add that to sources on our final doc, nt here -scarlett
-        image:"map.gif",
-        button: ""
+        image:"map.gif"
+       // button: ""
     },
 
     noafterparty: {
@@ -62,8 +62,8 @@ rooms = {
         description: "'Maybe next quarter..' *lights out*",
         exitKeys: ["closedinvitation"],
         exitTexts: ["Restart"],
-        image:"1.0home.gif",
-        button: ""
+        image:"1.0home.gif"
+       // button: ""
 
     },
 
@@ -72,8 +72,8 @@ rooms = {
         description: "Head up to Cowell and Stevenson",
         exitKeys: ["cands"],
         exitTexts: ["The uphill walk gives you a leg cramp."],
-        image:"1.1partybg.gif",
-        button: ""
+        image:"1.1partybg.gif"
+       // button: ""
     },
 
     cands: {
@@ -81,8 +81,8 @@ rooms = {
         description: "Welcome to Cowell and Steveson college. Cowell Stevenson is home to the Eloise Pickard Smith Gallery as well as home to our Humanities department",
         exitKeys: ["merrill"],
         exitTexts: ["The Gallery is closed this late but you make note of its hours to visit again later!"],
-        image:"2.3cowsteve.gif",
-        button: ""
+        image:"2.3cowsteve.gif"
+        //button: ""
     },
 
     merrill: {
@@ -90,8 +90,8 @@ rooms = {
         description: "Hey, glad you got up the hill, Merrill is known for cardiac hill.",
         exitKeys: ["crown"],
         exitTexts: ["'Strenuous workout' indeed- you feel like you're going into cardiac arrest."],
-        image:"2.4merrill.gif",
-        button: ""
+        image:"2.4merrill.gif"
+       // button: ""
     },
 
     crown: {
@@ -99,8 +99,8 @@ rooms = {
         description: "Oh no!!! There's a fire, you better make it up to the UCSC fire station at Crown college!",
         exitKeys: ["c9"],
         exitTexts: ["Oh wait it was a false alarm-someone in the dorms burnt their pizza pockets... but it's better to be safe than sorry!"],
-        image:"2.5crown.gif",
-        button: ""
+        image:"2.5crown.gif"
+       // button: ""
     },
 
     c9: {
@@ -108,8 +108,8 @@ rooms = {
         description: "Phew. You did a good job making it out of the would-be pizza pocket disaster of '23... Why don't you head over to the famous dining hall down at college nine",
         exitKeys: ["jrl"],
         exitTexts: ["It closed at 11! You barely make it but have enough time to grab a few of your own pizza pockets..."],
-        image:"2.6c9.gif",
-        button: ""
+        image:"2.6c9.gif"
+       // button: ""
     },
 
     jrl: {
@@ -117,8 +117,8 @@ rooms = {
         description: "Congratulations. Welcome to JRL, home to the University Center",
         exitKeys: ["kresge"],
         exitTexts: ["You feel a strong sense of community and take a moment to bask in it before moving on. The pizza pockets burn your mouth a little."],
-        image:"2.7jrl.gif",
-        button: ""
+        image:"2.7jrl.gif"
+        //button: ""
     },
 
     kresge: {
@@ -126,8 +126,8 @@ rooms = {
         description: "Welcome to Kresge. Here you can make your way through the forest to the campus RV park.",
         exitKeys: ["porter"],
         exitTexts: ["Bigfoot totally lives here."],
-        image:"2.8kresge.gif",
-        button: ""
+        image:"2.8kresge.gif"
+        //button: ""
     },
 
     porter: {
@@ -135,8 +135,8 @@ rooms = {
         description: "You made it to Porter! The arts division. Now you can really have fun, head over to Porter Meadow.",
         exitKeys: ["portermeadows"],
         exitTexts: ["You can see the party!"],
-        image:"2.9porter.gif",
-        button: ""
+        image:"2.9porter.gif"
+        //button: ""
     },
 
     portermeadows: {
@@ -144,8 +144,8 @@ rooms = {
         description: "Welcome to the Meadow! There is a huge party with dancing and loud music! After showing off your amazing breakdancing skills for a bit, you strike up some conversation with your peers around you. They love your dance moves! You talk with the group for a while and eventually someone offers you some funky pills. 'Elevates the Santa Cruz Slug Experience' they say. They leave it up for you to decide, which is cool because peer pressure is lame. You think about it for a moment...What do you do?.",
         exitKeys: ["takedrugs","nodrugs"],
         exitTexts: ["When in Rome!", "Nah, Thanks but not my thing."],
-        image:"3.0porterparty.gif",
-        button: ""
+        image:"3.0porterparty.gif"
+        //button: ""
     },
 
     takedrugs: {
@@ -153,8 +153,8 @@ rooms = {
         description: "What's the worst thing that could happen! This is what college is about right?",
         exitKeys: ["bananaslug"],
         exitTexts: ["Whoaaaa- You start to feel funnnnnnnnnnnyyyyyyyyyyy"],
-        image:"3.1takedrugs.gif",
-        button: "Drug Trip"
+        image:"3.1takedrugs.gif"
+       // button: "Drug Trip"
     },
 
     nodrugs: {
@@ -162,8 +162,8 @@ rooms = {
         description: "Hmmm, I think I'll pass.",
         exitKeys: ["leaveparty"],
         exitTexts:["You spend a few more hours dancing into the early morning with your new group of friends! Some of them are even in the same college as you! You collect contact information and safely Uber home because you are a responsible young adult."],
-        image:"3.0keeppartying.gif",
-        button: ""
+        image:"3.0keeppartying.gif"
+        //button: ""
     },
 
     bananaslug: {
@@ -171,8 +171,8 @@ rooms = {
         description:"Your vision begins to fog and you blink a few times before looking around. Everything seems larger around you. You feel like te world is in slow motion...What the heck! You are small and in slow motion! With a growing frustration you realize you've been turned into a banana slug! The mascot of the university! This wasn't what you thought it meant to elevate the Santa Cruz Slug experience! Oh well.. " ,
         exitKeys:["closedinvitation"],
         exitTexts:["Slug Ending-Restart"],
-        image:"6.1bananaslug.gif",
-        button: ""
+        image:"6.1bananaslug.gif"
+        //button: ""
       },
 
       leaveparty: {
@@ -180,8 +180,8 @@ rooms = {
         description:"As you lay in bed, content in your covers and ready for sleep, you glance over at your alarm clock. It reads 4:26 A.M. Your heart immediately sinks as you remember that your first 8am class starts today. Welcome to the University of Santa Cruz, Banana Slug!",
         exitKeys:["closedinvitation"],
         exitTexts:["True Ending-Restart"],
-        image:"7.0lastbushome.gif",
-        button: ""
+        image:"7.0lastbushome.gif"
+        //button: ""
       },
     }
 /////////////////////////HARD STOP//////////////////////////////////////////
@@ -213,6 +213,7 @@ function displayCurrentRoom(roomObj) {
     display("<div class='room-image'><img src='img/" + roomObj.image + "'></div>");
     display("<p class='title'>" + roomObj.name + "</p>");
     display("<p class='description'>" + roomObj.description + "</p>");
+    display("<button>" + roomObj.button + "</button>");
 }
 
 //takes a room object and displays the exit keys
@@ -241,7 +242,9 @@ function newRoom(nextRoom) {
 /////////////////////////////Drug trip//////////////////////////////////////////////
 //see if you can add a button into the array to change colors during the drug trip
 //ask ilia
-//$(takedrugs).html(roomObject.button) 
+//Object.onCLick= function(){
+    //document.getElementById('drugtrip')
+//};
 //load webpage and display the first room -MAKE SURE IT MATCHES CORRECTLY
 $(document).ready(function() {
     newRoom('closedinvitation');
